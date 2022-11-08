cad2029176ad45b7b177854aa3682177fd556c30
Redbird(Shihhung) Wu <redbird.wu@gmail.com>
2022-11-08 21:30:23 +0800

    update browser

--- silicbat.py
+++ silicbat.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-import numpy as np, pandas as pd, torch, cv2, os, argparse, math
+import numpy as np, pandas as pd, torch, cv2, os, time, shutil, sys
 import matplotlib.pyplot as plt
-from matplotlib.colors import ListedColormap, LinearSegmentedColormap
+from matplotlib.colors import ListedColormap
 from matplotlib import cm
 from pydub import AudioSegment, effects, scipy_effects
 from nnAudio import Spectrogram
@@ -22,7 +22,7 @@ def speed_change(sound, speed=1.0):
     print(sound_with_altered_frame_rate.frame_rate)
     return sound_with_altered_frame_rate.set_frame_rate(int(sound.frame_rate*speed))
 
-def AudioStandarize(audio_file, sr=320000, device=None, high_pass=0):
+def AudioStandarize(audio_file, sr=320000, device=None, high_pass=0, ultrasonic=False):
   if not device:
     device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
   filext = audio_file[-3:].lower()
@@ -43,6 +43,13 @@ def AudioStandarize(audio_file, sr=320000, device=None, high_pass=0):
     return None
   original_metadata = {'channel': sound.channels, 'sample_rate':sound.frame_rate, 'sample_size':len(sound.get_array_of_samples()), 'duration':sound.duration_seconds}
   print('Origional audio: channel = %s, sample_rate = %s Hz, sample_size = %s, duration = %s s' %(original_metadata['channel'], original_metadata['sample_rate'], original_metadata['sample_size'], original_metadata['duration']))
+  if ultrasonic:
+      if sound.frame_rate > 100000: # UltraSonic
+          sound = speed_change(sound, 1/10)
+      else:
+          return False
+  if sound.frame_rate > sr:
+      sound = scipy_effects.low_pass_filter(sound, sr/2)
   if sound.frame_rate != sr:
       sound = sound.set_frame_rate(sr)
   if sound.channels > 1:
@@ -53,21 +60,21 @@ def AudioStandarize(audio_file, sr=320000, device=None, high_pass=0):
     sound = sound.high_pass_filter(high_pass)
   sound = effects.normalize(sound) # normalize max-amplitude to 0 dB
   songdata = np.array(sound.get_array_of_samples())
-  duration = round(np.array(sound.get_array_of_samples()).shape[0]/sound.frame_rate*1000) #ms
+  duration = round(songdata.shape[0]/sound.frame_rate*1000) #ms
   audiodata = torch.tensor(songdata, device=device).float()
-  print('Standarized audio: channel = %s, sample_rate = %s Hz, sample_size = %s, duration = %s s' %(sound.channels, sound.frame_rate, len(sound.get_array_of_samples()), sound.duration_seconds))
+  print('Standarized audio: channel = %s, sample_rate = %s Hz, sample_size = %s, duration = %s s' %(sound.channels, sound.frame_rate, songdata.shape[0], sound.duration_seconds))
   return sound.frame_rate, audiodata, duration, sound, original_metadata
 
 class SilicBat:
   """
     Arguments:
-        sr (int): path of the model
-        n_fft (int): path of the config file
-        hop_length (str): select device that model will be loaded (cpu, cuda)
-        n_mels (int): load pretrained weights into the model
-        fmin (int): make model ready for inference
-        fmax (int): if False, yolov5 logs will be silent
-        clip_length (int):
+        sr (int): Sampling Rate in Hz
+        n_fft (int): Window(Frame) Size in samples
+        hop_length (str): Frame Step (or Hop Size) in samples
+        n_mels (int): The number of Mel filter banks
+        fmin (int): The starting frequency for the lowest Mel filter bank in Hz
+        fmax (int): The ending frequency for the highest Mel filter bank in Hz
+        clip_length (int): The duration of each inference in ms
   """
   def __init__(self, sr=320000, n_fft=800, hop_length=320, n_mels=128, fmin=16000, fmax=150000, device=None, clip_length=128):
     self.sr = sr
@@ -87,13 +94,14 @@ class SilicBat:
     self.model_path = None
     self.model = None
     self.names = None
+    self.soundclasses = None
   
-  def audio(self, audio_file):
+  def audio(self, audio_file, ultrasonic=False):
     self.audiofilename = os.path.basename(audio_file)
     self.audiofilename_without_ext = os.path.splitext(self.audiofilename)[0]
     self.audiopath = os.path.dirname(audio_file)
     self.audiofileext = audio_file.split('.')[-1]
-    self.sr, self.audiodata, self.duration, self.sound, self.original_metadata = AudioStandarize(audio_file, self.sr, self.device, high_pass=self.fmin)
+    self.sr, self.audiodata, self.duration, self.sound, self.original_metadata = AudioStandarize(audio_file, self.sr, self.device, high_pass=self.fmin, ultrasonic=ultrasonic)
 
   def save_standarized(self, targetmp3path=None):
     if not targetmp3path:
@@ -104,16 +112,7 @@ class SilicBat:
     print('Standarized audio was saved to %s' %targetmp3path)
     return targetmp3path
     
-  def spectrogram(self, audiodata, spect_type='linear', rainbow_bands=8):
-    plt.rcParams['font.size'] = '16'
-    plt.rcParams['axes.grid'] = False
-    plt.rcParams['xtick.labelsize'] = False
-    plt.rcParams['ytick.labelsize'] = False
-    plt.rcParams['xtick.top'] = False
-    plt.rcParams['xtick.bottom'] = False
-    plt.rcParams['ytick.left'] = False
-    plt.rcParams['ytick.right'] = False
-    plt.rcParams.update({'font.size': 16})
+  def spectrogram(self, audiodata, spect_type='linear', rainbow_bands=5):
     if spect_type in ['mel', 'rainbow']:
       spec = self.spec_mel_layer(audiodata)
       w = spec.size()[2]/55
@@ -145,6 +144,15 @@ class SilicBat:
       plt.subplots_adjust(top=1, bottom=0, right=1, left=0, hspace=0, wspace=0)
       plt.imshow(data, origin='lower', cmap='gray_r', aspect='auto')
     
+    """
+    plt.savefig(targetfilepath)
+    if show:
+      plt.show()
+    
+
+    if spect_type == 'rainbow' and rainbow_bands == 5:
+      self.rainbow_img = self.cv2_img
+    """
     fig.canvas.draw()
     img = np.frombuffer(fig.canvas.tostring_rgb(), dtype=np.uint8)
     img = img.reshape(fig.canvas.get_width_height()[::-1] + (3,))
@@ -153,15 +161,14 @@ class SilicBat:
     
     return cv2_img
 
-  def tfr(self, targetfilepath=None, spect_type='linear', rainbow_bands=8, start=0, stop=None):
+  def tfr(self, targetfilepath=None, spect_type='linear', rainbow_bands=5, start=0, stop=None):
     if self.clip_length and ((self.audiodata.size()[0] / self.sr * 1000) < self.clip_length):
         self.audiodata = torch.cat((self.audiodata, torch.zeros(round(self.clip_length*self.sr/1000)-self.audiodata.size()[0], device=self.device)), 0)
     if not stop:
         stop = self.duration
     max_sample_size = 1920000
-    tmpimgs = []
     if not targetfilepath:
-      targetfilepath = os.path.join(self.audiopath, spect_type, '%s.png'%self.audiofilename_without_ext)
+      targetfilepath = os.path.join(self.audiopath, spect_type, '%s.jpg'%self.audiofilename_without_ext)
       if not os.path.isdir(os.path.dirname(targetfilepath)):
         os.mkdir(os.path.dirname(targetfilepath))
     if not os.path.isdir(os.path.dirname(targetfilepath)):
@@ -184,13 +191,13 @@ class SilicBat:
             try:
               imgs.append(self.spectrogram(data, spect_type, rainbow_bands=rainbow_bands))
             except:
-              print('error in converting tmpimg')
+              print('error in converting')
               exit()
         self.cv2_img = cv2.hconcat(imgs)
     else:
         self.cv2_img = self.spectrogram(self.audiodata[int(round(start/1000*self.sr)):int(round(stop/1000*self.sr))], spect_type, rainbow_bands=rainbow_bands)
     
-    if spect_type == 'rainbow' and rainbow_bands == 8:
+    if spect_type == 'rainbow' and rainbow_bands == 5:
       self.rainbow_img = cv2.cvtColor(self.cv2_img, cv2.COLOR_RGB2BGR)
     
     height, width, colors = self.cv2_img.shape
@@ -218,15 +225,18 @@ class SilicBat:
     fh = self.mel_to_freq(1-(y-h/2))
     return [ts, te, fl, fh]
 
-  def detect(self, weights, step=64, conf_thres=0.1, imgsz=640, targetfilepath=None, iou_thres=0.25, soundclasses=None):
+  def detect(self, weights, step=100, conf_thres=0.1, imgsz=640, targetfilepath=None, iou_thres=0.25, targetclasses=None):
     if self.model and self.model_path == weights:
       pass
     else:
       self.model_path = weights
-      self.model = attempt_load(self.model_path, map_location=self.device)
-      self.names = self.model.module.names if hasattr(self.model, 'module') else self.model.names
-    if soundclasses:
-      classes = [self.names.index(name) for name in soundclasses]
+      model = attempt_load(self.model_path, map_location=self.device)
+      self.names = model.module.names if hasattr(model, 'module') else model.names
+      model.float()
+      self.model = model
+      self.soundclasses = pd.read_csv(self.model_path.replace('best.pt', 'soundclasses.csv'), encoding='utf8', index_col='sounclass_id').T.to_dict()
+    if targetclasses:
+      classes = [self.names.index(name) for name in targetclasses]
     else:
       classes = None
     self.tfr(targetfilepath=targetfilepath, spect_type='rainbow')
@@ -246,7 +256,7 @@ class SilicBat:
       dataset.append([os.path.join(self.audiopath, self.audiofilename), img, img0, ts])
     
     
-    labels = [['file', "time_begin", "time_end", "freq_low", "freq_high", 'classid', "score"]]
+    labels = [['file', 'classid', 'species_name', 'sound_class', 'scientific_name', "time_begin", "time_end", "freq_low", "freq_high", "score"]]
     for path, img, im0, time_start in dataset:
       img = torch.from_numpy(img).float().to(self.device)
       img /= 255.0  # 0 - 255 to 0.0 - 1.0
@@ -263,7 +273,11 @@ class SilicBat:
             xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()    # normalized xywh
             ttff = self.xywh2ttff(xywh)
             ts, te, fl, fh = ttff
-            labels.append([path, round(time_start+ts), round(time_start+te), fl, fh, self.names[int(cls)], round(float(conf),3)])
+            classid = self.names[int(cls)]
+            species_name = self.soundclasses[classid]['species_name']
+            sound_class = self.soundclasses[classid]['sound_class']
+            scientific_name = self.soundclasses[classid]['scientific_name']
+            labels.append([path, classid, species_name, sound_class, scientific_name, round(time_start+ts), round(time_start+te), fl, fh, round(float(conf),3)])
     
     return labels
     
@@ -330,7 +344,7 @@ def merge_boxes(bb1, bb2):
     y2 = bb2['y2']
   return {'x1':x1, 'x2':x2, 'y1':y1, 'y2':y2}
 
-def clean_multi_boxes(labels, threshold_iou=0.25, threshold_iratio=0.75):
+def clean_multi_boxes(labels, threshold_iou=0.25, threshold_iratio=0.5):
   df = pd.DataFrame(labels[1:],columns=labels[0])
   df = df.sort_values('time_begin')
   df_results = pd.DataFrame()
@@ -364,18 +378,18 @@ def clean_multi_boxes(labels, threshold_iou=0.25, threshold_iratio=0.75):
           break
       if check:
         if df_results.shape[0] > 0:
-          df_results = pd.concat([df_results, df_class[df_class.index == i]], axis=0, ignore_index = True)
+          df_results = df_results.append(df_class[df_class.index == i], ignore_index = True)
         else:
           df_results = df_class[df_class.index == i]
   return df_results.sort_values('time_begin').reset_index(drop=True)
 
 def draw_labels(silic, labels, outputpath=None):
   if outputpath and os.path.isdir(outputpath):
-    targetpath = os.path.join(outputpath, '%s.png'%silic.audiofilename_without_ext)
+    targetpath = os.path.join(outputpath, '%s.jpg'%silic.audiofilename_without_ext)
   else:
     if not os.path.isdir(os.path.join(silic.audiopath, 'labels')):
       os.mkdir(os.path.join(silic.audiopath, 'labels'))
-    targetpath = os.path.join(silic.audiopath, 'labels', '%s.png'%silic.audiofilename_without_ext)
+    targetpath = os.path.join(silic.audiopath, 'labels', '%s.jpg'%silic.audiofilename_without_ext)
   outputimage = silic.tfr()
   img_pil = Image.open(outputimage)
   width, height = img_pil.size
@@ -387,7 +401,7 @@ def draw_labels(silic, labels, outputpath=None):
     x2 = round(label['time_end']/silic.duration*width)
     y1 = round((1-label['freq_high']/(silic.sr/2))*height)
     y2 = round((1-label['freq_low']/(silic.sr/2))*height)
-    tag = '%s(%.3f)' %(label['classid'], label['score'])
+    tag = '%s%s(%.3f)' %(label['species_name'], label['sound_class'], label['score'])
     draw.text((x1, y1-12),  tag, font = font, fill = 'red')
     draw.rectangle(((x1, y1), (x2, y2)), outline='red')
   try:
@@ -398,3 +412,110 @@ def draw_labels(silic, labels, outputpath=None):
   #img_pil.show()
   print(targetpath, 'saved')
   return targetpath
+
+
+def browser(audiosource, weights='model/exp/best.pt', step=100, targetclasses=[], conf_thres=0.1, savepath=None, zip=True):
+  t0 = time.time()
+  # init
+  if savepath and os.path.isdir(savepath):
+    result_path = savepath
+  else:
+    result_path = 'result_silic'
+  if os.path.isdir(audiosource) and audiosource == savepath:
+    audio_path = None
+  else:
+    audio_path = os.path.join(result_path, 'audio')
+  linear_path = os.path.join(result_path, 'linear')
+  rainbow_path = os.path.join(result_path, 'rainbow')
+  lable_path = os.path.join(result_path, 'label')
+  js_path = os.path.join(result_path, 'js')
+  #if os.path.isdir(result_path):
+  #  shutil.rmtree(result_path, ignore_errors=True)
+  if not os.path.isdir(result_path):
+    os.mkdir(result_path)
+  if audio_path and not os.path.isdir(audio_path):
+    os.mkdir(audio_path)
+  if not os.path.isdir(linear_path):
+    os.mkdir(linear_path)
+  if not os.path.isdir(rainbow_path):
+    os.mkdir(rainbow_path)
+  if not os.path.isdir(lable_path):
+    os.mkdir(lable_path)
+  if not os.path.isdir(js_path):
+    os.mkdir(js_path)
+  shutil.copyfile('browser/index.html', os.path.join(result_path, 'index.html'))
+  all_labels = pd.DataFrame()
+  model = SilicBat()
+  audiofile = None
+  if os.path.isfile(audiosource):
+    sourthpath = ''
+    audiofiles = [audiosource]
+  elif os.path.isdir(audiosource):
+    sourthpath = audiosource
+    audiofiles = os.listdir(audiosource)
+    print(len(audiofiles), 'files found.')
+  else:
+    print('Files not found')
+    exit()
+  i = 0
+  for audiofile in audiofiles:
+    audiofile = os.path.join(sourthpath, audiofile)
+    if not audiofile.split('.')[-1].lower() in ['mp3', 'wma', 'm4a', 'ogg', 'wav', 'mp4', 'wma', 'aac']:
+      continue
+    model.audio(audiofile)
+    i += 1
+    if audio_path:
+      shutil.copyfile(audiofile, os.path.join(audio_path, model.audiofilename))
+      #model.save_standarized(targetmp3path=os.path.join(audio_path, model.audiofilename.replace('.wav','.mp3').replace('.WAV','.mp3')))
+    model.tfr(targetfilepath=os.path.join(linear_path, model.audiofilename_without_ext+'.png'))
+    labels = model.detect(weights=weights, step=step, targetclasses=targetclasses, conf_thres=conf_thres, targetfilepath=os.path.join(rainbow_path, model.audiofilename_without_ext+'.png'))
+    if len(labels) == 1:
+      print("No sound found in %s." %audiofile)
+    else:
+      newlabels = clean_multi_boxes(labels)
+      newlabels['file'] = model.audiofilename
+      newlabels.to_csv(os.path.join(lable_path, model.audiofilename_without_ext+'.csv'), index=False)
+      if all_labels.shape[0] > 0:
+        all_labels = all_labels.append(newlabels, ignore_index = True)
+      else:
+        all_labels = newlabels
+      print("%s sounds of %s species is/are found in %s" %(newlabels.shape[0], len(newlabels['classid'].unique()), audiofile))
+
+  if all_labels.shape[0] == 0:
+    print('No sounds found!')
+  else:
+    all_labels.to_csv(os.path.join(lable_path, 'labels.csv'), index=False)
+    print('%s sounds of %s species is/are found in %s recording(s). Preparing the browser package ...' %(all_labels.shape[0], len(all_labels['classid'].unique()), i))
+    df_classes = pd.read_csv(weights.replace('best.pt', 'soundclasses.csv'))
+    if targetclasses:
+      df_classes = df_classes[df_classes['sounclass_id'].isin(targetclasses)]
+    else:
+      names = all_labels['classid'].unique()
+      df_classes = df_classes[df_classes['sounclass_id'].isin(names)]
+    with open(os.path.join(js_path, 'soundclass.js'), 'w', newline='', encoding='utf-8') as csv_file:
+      csv_file.write('var sounds = { \n')
+      for index, row in df_classes.iterrows():
+        csv_file.write('"%s": ["%s", "%s", "%s"], \n' %(row['sounclass_id'], row['species_name'], row['sound_class'], row['scientific_name']))
+      csv_file.write('};')
+
+    with open(os.path.join(js_path, 'labels.js'), 'w', newline='', encoding='utf-8') as f:
+      f.write('var  labels  =  [' + '\n')
+      for index, label in all_labels.iterrows():
+        f.write("['{}', {}, {}, {}, {}, {}, {}],\n".format(label['file'], label['time_begin'], label['time_end'], label['freq_low'], label['freq_high'], label['classid'], label['score']))
+      f.write('];' + '\n')
+    
+    if zip:
+        shutil.make_archive('result_silic', 'zip', result_path)
+        print('Finished. The browser package is compressed and named result.zip')
+    else:
+        print('Finished. All results were saved in the folder %s' %result_path)
+    print(time.time()-t0, 'used.')
+
+if __name__ == '__main__':
+  mainpath = sys.argv[1]
+  if os.path.isfile(mainpath):
+    browser(mainpath, zip=False)
+  else:
+    for dirPath, dirNames, fileNames in os.walk(mainpath):
+      for dir in dirNames:
+        browser(os.path.join(dirPath,dir), zip=False)
